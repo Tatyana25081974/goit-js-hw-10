49d8fc9becc31a93c71f1cddf1185103c69843c0
Tatyana25081974 <tatyanakorabelnikovamama@gmail.com>
2025-01-10 16:39:34 +0000

    Deploying to gh-pages from @ Tatyana25081974/goit-js-hw-10@1f138c813f1c15f90f79587119d319403376411b 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as o}from"./assets/vendor-BbSUbo7J.js";document.querySelector(".form").addEventListener("submit",s=>{s.preventDefault();const i=s.target,t=Number(i.delay.value),r=i.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(t):m(t)},t)}).then(e=>{o.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`,position:"topRight",timeout:5e3})}).catch(e=>{o.error({title:"❌ Error",message:`Rejected promise in ${e}ms`,position:"topRight",timeout:5e3})})});
+//# sourceMappingURL=2-snackbar.js.map
